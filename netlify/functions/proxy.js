1d50e4dbc7676f82c6865b9d8f0904d318c7eb02
CEAtBowl <lfreestyler45@gmail.com>
2025-07-06 17:09:12 +0200

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -9,7 +9,7 @@ export async function handler(event, context) {
   const data = JSON.parse(event.body);
 
   try {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbwT7Z0J0ioT-nq0F_tXzhJzADDTqN1zbrN30DoAx_FOaeh6RH7CJgmUminYXAc5yCY/exec', {
+    const response = await fetch('https://script.google.com/macros/s/AKfycby8vpuVMkrQSvuvRXcGtSs8l0e7u9qHOLoqphmvNokpW_W1XtwX-A5h12Vw73_Wzhc/exec', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
